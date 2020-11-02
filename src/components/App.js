76334319e69710be81236c0c752174be2b8983db
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-02 14:56:36 +0530

    Test4

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,24 @@ class App extends Component {
     super(props);
     this.intervalId = null;
     this.state = {
-      time: new Date().toLocaleDateString("en-US")
+      time: new Date().toLocaleDateString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
     };
   }
 
   componentDidMount() {
     this.intervalId = setInterval(() => {
       this.setState({
-        time: new Date().toLocaleDateString("en-US")
+        time: new Date().toLocaleDateString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: true
+        })
       });
     }, 1000);
   }
