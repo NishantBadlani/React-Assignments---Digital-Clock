5c34aaab44d9233282665fe3d17455f39e102f03
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-02 14:46:12 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,24 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.intervalId = null;
     this.state = {
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-      })
+      time: new Date().toLocaleDateString("en-US")
     };
-  }
-  tick() {
-    this.setState({
-      time: new Date().toLocaleTimeString("en-US", {
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: true
-      })
-    });
   }
 
   componentDidMount() {
-    this.intervalID = setInterval(() => this.tick(), 1000);
+    this.intervalId = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleDateString("en-US")
+      });
+    }, 1000);
   }
+
   componentWillUnmount() {
-    clearInterval(this.intervalID);
+    clearInterval(this.intervalId);
   }
+
   render() {
     return (
       <div className="App">
