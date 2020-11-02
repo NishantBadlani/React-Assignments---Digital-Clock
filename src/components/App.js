810d8b30c22d543c103e8086b6e18f81d32319ef
NishantBadlani <nishant.03badlani@gmail.com>
2020-11-02 14:34:11 +0530

    FinalChanges

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,8 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.intervalId = null;
     this.state = {
-      time: new Date().toLocaleDateString("en-US", {
+      time: new Date().toLocaleTimeString("en-US", {
         hour: "numeric",
         minute: "numeric",
         second: "numeric",
@@ -14,29 +13,28 @@ class App extends Component {
       })
     };
   }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
 
   componentDidMount() {
-    this.intervalId = setInterval(() => {
-      this.setState({
-        time: new Date().toLocaleDateString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: true
-        })
-      });
-    }, 1000);
+    this.intervalID = setInterval(() => this.tick(), 1000);
   }
-
   componentWillUnmount() {
-    clearInterval(this.intervalId);
+    clearInterval(this.intervalID);
   }
-
   render() {
     return (
       <div className="App">
         <div className="Clock">
-          <h3 id="time">{this.state.time}</h3>
+          <h3 id="time">{`${this.state.time}`}</h3>
         </div>
       </div>
     );
